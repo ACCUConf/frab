cf73653d33a6cfd8afd839ed3c9526496c22319d
ACCU Conference <conference@dennis.default.accu.uk0.bigv.io>
2022-09-28 18:26:04 +0100

    Improve SMTP configuration options

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -3,17 +3,34 @@ unless Rails.env.development?
     host: ENV.fetch('FRAB_HOST'),
     protocol: ENV.fetch('FRAB_PROTOCOL')
   }
-  if ENV['SMTP_ADDRESS']
-    %w(ADDRESS PORT DOMAIN USER_NAME PASSWORD AUTHENTICATION).each do |setting|
-      next unless ENV["SMTP_#{setting}"].present?
+  
+  ActionMailer::Base.delivery_method = :smtp 
+  # Undo all setting that we created by using the ActionMailer before the actual initialization was done
+  ActionMailer::Base.smtp_settings = {} 
+  %w(ADDRESS PORT DOMAIN USER_NAME PASSWORD AUTHENTICATION ENABLE_STARTTLS ENABLE_STARTTLS_AUTO SSL TLS OPENSSL_VERIFY_MODE).each do |setting|
+    next unless ENV["SMTP_#{setting}"].present?
+    case setting.downcase
+    when "port"
+      ActionMailer::Base.smtp_settings[setting.downcase.to_sym] = ENV["SMTP_#{setting}"].to_i
+    when "openssl_verify_mode"
+      if %w(none peer).include? ENV["SMTP_#{setting}"].downcase
+        ActionMailer::Base.smtp_settings[setting.downcase.to_sym] = ENV["SMTP_#{setting}"].downcase
+      end
+    when "ssl", "tls", "enable_starttls", "enable_starttls_auto"
+      ActionMailer::Base.smtp_settings[setting.downcase.to_sym] = ENV["SMTP_#{setting}"].downcase == "true"
+    else 
       ActionMailer::Base.smtp_settings[setting.downcase.to_sym] = ENV["SMTP_#{setting}"]
     end
   end
+
   if ENV.fetch('SMTP_NOTLS', 'false') == 'true'
     ActionMailer::Base.smtp_settings.merge!(
       enable_starttls_auto: false,
+      openssl_verify_mode: :none,
       ssl: false,
       tls: false
     )
   end
+  Rails.logger.info "========> Env #{ENV.keys}"
+  Rails.logger.info "!!!!!!!! #{ActionMailer::Base.smtp_settings}"
 end
